94a3f1bf944d3252633cc593e60b74705e95dfb4
mohamed <khaldimohamed82@gmail.com>
2024-12-26 03:06:10 +0100

    Expanding my Skillset, Embracing Continuous Learning

---
+++ main.c
@@ -0,0 +1,19 @@
+//#include <get_next_line.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int	main(void)
+{
+	char	str[256];
+	int	read_char;
+	int		fd;
+
+	fd = open("text.txt", O_RDONLY);
+	while ((read_char = read(fd, str, 5)))
+	{
+		str[read_char] = '\0';
+		printf("red -> %s\n", str);
+	}
+	return (0);
+}
