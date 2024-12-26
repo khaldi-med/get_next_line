5c0b5ef97b9a5750c9eb168bda6ccb444dc68e47
mohamed <khaldimohamed82@gmail.com>
2024-12-26 02:22:21 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,15 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 02:18:27 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/12/26 02:21:10 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/12/26 02:22:18 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef		GET_NEXT_LINE_H
-#define		GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <unistd.h>
+# include <unistd.h>
 
-get_next_line_utils(int fd);
+get_next_line(int fd);
 
 #endif
