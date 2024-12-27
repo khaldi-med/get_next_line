2f6509419613a1181ddcae9b4b0343400286c427
mohkhald <mohkhald@student.1337.ma>
2024-12-27 06:55:43 +0100

    mohkhald

--- main.c
+++ main.c
@@ -1,51 +1,55 @@
+#include "get_next_line.h"
 #include <fcntl.h>
-#include <get_next_line.h>
 #include <stdio.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 5
 
-char ft_get_line(char *str, int fd){
-	char *buffer;
-	int bit_read = 0;
-	
+char	*ft_get_line(char *str, int fd)
+{
+	char	*buffer;
+	int		bit_read;
+
+	bit_read = 0;
 	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if(!buffer)
-		return NULL;
-	if(!str)
+	if (!buffer)
+		return (NULL);
+	if (!str)
 		str = ft_strdup("");
-	²
-
-	while((bit_read = read(fd, buffer, BUFFER_SIZE)){
-		buffer[bit_read] = '\0';
-		printf
+	while ((bit_read = read(fd, buffer, BUFFER_SIZE)))
+	{
+		str[bit_read] = buffer[bit_read];
 	}
-}
-char *get_next_line(int fd){
-	char *buffer;
-	int bit_read = 0;
-	if(!BUFFER_SIZE || read(fd, 0, 0) < 0)
-		return NULL;
-	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if(!buffer)
-		return NULL;
-
-			
-		
+	str[bit_read] = '\0';
+	return (str);
 }
 
+// char	ft_clean_line(void)
+// {
+// }
+//
+// char	*ft_get_next_line(int fd)
+// {
+// 	char	*buffer;
+// 	int		bit_read;
+//
+// 	bit_read = 0;
+// 	if (!BUFFER_SIZE || read(fd, 0, 0) < 0)
+// 		return (NULL);
+// 	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+// 	if (!buffer)
+// 		return (NULL);
+// }
 
 int	main(void)
 {
-	char	str[256];
-	int		read_char;
+	char	*str;
+	char	*res;
 	int		fd;
 
+	str = NULL;
 	fd = open("text.txt", O_RDONLY);
-	while ((read_char = read(fd, str, 5)))
-	{
-		str[read_char] = '\0';
-		printf("red-> %s\n", str);
-	}
+	res = ft_get_line(str, fd);
+	printf("red-> %s\n", res);
 	return (0);
 }
