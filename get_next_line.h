1c047ff43ed49dcb1b8b6bc3c4143ae0aa11d39e
mohamed <khaldimohamed82@gmail.com>
2024-12-28 14:02:18 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mohkhald <mohkhald@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 02:18:27 by mohkhald          #+#    #+#             */
-/*   Updated: 2024/12/27 01:34:32 by mohkhald         ###   ########.fr       */
+/*   Updated: 2024/12/28 12:26:34 by mohkhald         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strdup(const char *s);
 char *ft_strchr(const char *str, int c);
 size_t	ft_strlen(const char *str);
-
+// char>---**ft_split(char const *s, char c);
 #endif
--- main.c
+++ main.c
@@ -1,51 +1,114 @@
+#include "get_next_line.h"
 #include <fcntl.h>
-#include <get_next_line.h>
 #include <stdio.h>
 #include <unistd.h>
 
-#define BUFFER_SIZE 5
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+#endif
 
-char ft_get_line(char *str, int fd){
-	char *buffer;
-	int bit_read = 0;
-	
-	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if(!buffer)
-		return NULL;
-	if(!str)
-		str = ft_strdup("");
-	²
+static char	*ft_check_newline(char *buffer)
+{
+	char	*line;
+	int		i;
+	int		j;
 
-	while((bit_read = read(fd, buffer, BUFFER_SIZE)){
-		buffer[bit_read] = '\0';
-		printf
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	line = malloc(sizeof(char) * (i + 1));
+	if (!line)
+		return (NULL);
+	j = -1;
+	while (++j < i)
+		line[j] = buffer[j];
+	line[j] = '\0';
+	return (line);
+}
+
+static char	*ft_save_rest(char *buffer)
+{
+	char	*rest;
+	int		i;
+	int		j;
+
+	i = 0;
+	while (buffer[i] && buffer[i] != '\n')
+		i++;
+	if (buffer[i] == '\n')
+		i++;
+	rest = malloc(sizeof(char) * (BUFFER_SIZE - i + 1));
+	if (!rest)
+		return (NULL);
+	j = 0;
+	while (buffer[i])
+		rest[j++] = buffer[i++];
+	rest[j] = '\0';
+	return (rest);
+}
+
+static char	*ft_get_line(int fd)
+{
+	char	*str;
+	char	*buffer;
+	int		bytes_read;
+	int		i;
+
+	i = -1;
+	if (fd < 0 || !str || BUFFER_SIZE < 0)
+		return (NULL);
+	bytes_read = 0;
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	if (bytes_read <= 0)
+	{
+		free(buffer);
+		return (NULL);
 	}
+	buffer[bytes_read] = '\0';
+	while (++i < bytes_read)
+		str[i] = buffer[i];
+	str[i] = '\0';
+	free(buffer);
+	return (str);
 }
-char *get_next_line(int fd){
-	char *buffer;
-	int bit_read = 0;
-	if(!BUFFER_SIZE || read(fd, 0, 0) < 0)
-		return NULL;
+
+char	*get_next_line(int fd)
+{
+	char	*buffer;
+	int		bytes_read;
+
+	bytes_read = 0;
+	if (!BUFFER_SIZE || read(fd, 0, 0) < 0)
+		return (NULL);
 	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if(!buffer)
-		return NULL;
-
-			
-		
+	if (!buffer)
+		return (NULL);
+	buffer = ft_get_line(fd)
+	buffer = ft_get_line
 }
-
 
 int	main(void)
 {
-	char	str[256];
-	int		read_char;
+	char	*res;
 	int		fd;
 
+	char str[256] = {0}; // Initialize to zero
 	fd = open("text.txt", O_RDONLY);
-	while ((read_char = read(fd, str, 5)))
+	if (fd < 0)
 	{
-		str[read_char] = '\0';
-		printf("red-> %s\n", str);
+		printf("Error opening file\n");
+		return (1);
 	}
+	res = ft_get_line(str, fd);
+	if (res)
+		printf("Read content: '%s'\n", res);
+	else
+		printf("Nothing read or error occurred\n");
+	close(fd);
 	return (0);
 }
