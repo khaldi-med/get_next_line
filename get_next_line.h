d86b48d426ee3d8e3307ebe2756934d4d6fa7d9e
mohamed <khaldimohamed82@gmail.com>
2025-01-05 21:35:37 +0100

    Expanding my Skillset, Embracing Continuous Learning

--- get_next_line.h
+++ get_next_line.h
@@ -10,7 +10,9 @@
 char	*get_next_line(int fd);
 // char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strdup(const char *s);
 char	*ft_strchr(const char *str, int c);
 size_t	ft_strlen(const char *str);
+
 #endif
